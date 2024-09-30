4ae867cb7bccc5ea2ceaaa07a5c7bc609d9a7c24
hao <cutecat8110@gmail.com>
2024-09-30 14:04:00 +0800

    build: 安裝並設定 gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // 開發中、產品路徑
+  base: process.env.NODE_ENV === "production" ? "/react-lab/" : "/",
   plugins: [react()],
-})
+});
